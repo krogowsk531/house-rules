43beec7db0801a514269de04dfe56a3b2b7cc6af
Hanna Kim <hannakim91@gmail.com>
2020-10-28 14:31:51 -0600

    Import and add Link back to home

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,6 +1,15 @@
+import Link from 'next/link'
+
 const NavBar = () => {
   return (
-    <>TEST</>
+    <>
+      {/*logo and header that link back to home */}
+      <h1>
+        <Link href="/">
+          <a>House Rules</a>
+        </Link>
+      </h1>
+    </>
   )
 }
 
