73fdcd1656cad0ba22e82b0f9e1be73b93ded82b
Hanna Kim <hannakim91@gmail.com>
2020-10-29 14:48:43 -0600

    Write async call for dynamic gameid pages

--- pages/GameDetails/[gameid].js
+++ pages/GameDetails/[gameid].js
@@ -1,18 +1,27 @@
 import { useRouter } from 'next/router'
-import { getSingleGame } from '../api/apiCalls.js'
+import { getGames } from '../api/apiCalls.js'
 
-const GameDetails = () => {
-  const router = useRouter()
-  const { gameid } = router.query
-  console.log(gameid)
-  // const data = await getSingleGame(gameid)
-  // console.log(data)
-  return <p>this is a detail page </p>
-}
-// should NOT use getInitialProps more than once (already in root index.js -- use other react hooks/react lifecycle methods)
-GameDetails.getInitialProps = async () => {
-  return {}
-}
-//** how to pass gameid into getinitialprops - does this involve hooks? query object in context param?*/
+// const GameDetails = () => {
+//   const router = useRouter()
+//   const { gameid } = router.query
+//   console.log(gameid)
+//   // const data = await getSingleGame(gameid)
+//   // console.log(data)
+//   return <p>this is a detail page </p>
+// }
+// // should NOT use getInitialProps more than once (already in root index.js -- use other react hooks/react lifecycle methods)
+// GameDetails.getInitialProps = async () => {
+//   return {}
+// }
+// //** how to pass gameid into getinitialprops - does this involve hooks? query object in context param?*/
 
-export default GameDetails
+// export default GameDetails
+
+export async function getStaticPaths() {
+  const games = await getGames()
+  const paths = games.map(game => {
+    params: { gameid: game.id }
+  })
+
+  return { paths, fallback: false }
+}
