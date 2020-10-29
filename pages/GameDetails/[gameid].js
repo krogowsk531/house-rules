8709aff6021b10e401d318131cf77c398a62b27a
Hanna Kim <hannakim91@gmail.com>
2020-10-29 14:28:57 -0600

    Refactor Home to functional component using getStaticProps

--- pages/GameDetails/[gameid].js
+++ pages/GameDetails/[gameid].js
@@ -1,11 +1,18 @@
 import { useRouter } from 'next/router'
+import { getSingleGame } from '../api/apiCalls.js'
 
 const GameDetails = () => {
   const router = useRouter()
   const { gameid } = router.query
-
-  return <p>this is a detail page {gameid}</p>
+  console.log(gameid)
+  // const data = await getSingleGame(gameid)
+  // console.log(data)
+  return <p>this is a detail page </p>
 }
-
+// should NOT use getInitialProps more than once (already in root index.js -- use other react hooks/react lifecycle methods)
+GameDetails.getInitialProps = async () => {
+  return {}
+}
+//** how to pass gameid into getinitialprops - does this involve hooks? query object in context param?*/
 
 export default GameDetails
