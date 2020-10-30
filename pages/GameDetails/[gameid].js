17415cabbdff252d19b07f4ed721259af5a44ab9
Hanna Kim <hannakim91@gmail.com>
2020-10-29 20:15:11 -0600

    Add HTML elements using fetched game data

--- pages/GameDetails/[gameid].js
+++ pages/GameDetails/[gameid].js
@@ -36,6 +36,18 @@ export async function getStaticProps({params}) {
 
 export default function GameDetails({ game }) {
   return (
-    <>{game.games[0].name}</>
+    <>
+      <h2>{game.games[0].name}</h2>
+      <img
+        src={game.games[0].image_url}
+        alt={game.games[0].name}
+      />
+      <p>{game.games[0].description_preview}</p>
+      <h3>Number of Players</h3>
+      <p>Min: {game.games[0].min_players} - Max: {game.games[0].max_players}</p>
+      <h3>Estimate Play Time</h3>
+      <p>{game.games[0].max_playtime}</p>
+      <h3><a href={game.games[0].rules_url}>Official Game Rules</a></h3>
+    </>
   )
 }
