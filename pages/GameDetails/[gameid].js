01cc0763179dd85a58077b6b9f8c4af9a00c4620
Hanna Kim <hannakim91@gmail.com>
2020-10-29 20:23:38 -0600

    Delete unused code

--- pages/GameDetails/[gameid].js
+++ pages/GameDetails/[gameid].js
@@ -1,21 +1,4 @@
-import { useRouter } from 'next/router'
 import { getGames, getSingleGame } from '../api/apiCalls.js'
-
-// const GameDetails = () => {
-//   const router = useRouter()
-//   const { gameid } = router.query
-//   console.log(gameid)
-//   // const data = await getSingleGame(gameid)
-//   // console.log(data)
-//   return <p>this is a detail page </p>
-// }
-// // should NOT use getInitialProps more than once (already in root index.js -- use other react hooks/react lifecycle methods)
-// GameDetails.getInitialProps = async () => {
-//   return {}
-// }
-// //** how to pass gameid into getinitialprops - does this involve hooks? query object in context param?*/
-
-// export default GameDetails
 
 export async function getStaticPaths() {
   const games = await getGames()
