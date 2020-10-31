4d1d7bdc94373edf2a0b7c713ddaabc7f27c5280
Katy St Sauveur <katyrogowski@yahoo.com>
2020-10-31 09:15:36 -0700

    Attempt to add hooks to form for local storage purposes

--- components/RuleDisputeForm/RuleDisputeForm.js
+++ components/RuleDisputeForm/RuleDisputeForm.js
@@ -1,38 +1,101 @@
+import React from 'react'
+import { useState } from 'react';
+
+// function App() {
+//   // Similar to useState but first arg is key to the value in local storage.
+//   const [name, setName] = useLocalStorage('name', 'Bob');
+//
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         placeholder="Enter your name"
+//         value={name}
+//         onChange={e => setName(e.target.value)}
+//       />
+//     </div>
+//   );
+// }
+
+
+
 const RuleDisputeForm = () => {
-  return(
-    <section>
+  const [expansion, setExpansion] = useLocalStorage('expansion', 'setExpansion')
+  // const [number, setNumber] = useLocalStorage('number', 'setNumber')
+    return(
+      <section>
       <form>
-      <label>Expansion Name: </label>
-      <input
+        <label>Expansion Name: </label>
+        <input
         type='text'
         name='expansion'
-      />
-      <label>Number of players: </label>
-      <input
+        value={expansion}
+        onChange={event =>setExpansion(event.target.value)}
+        />
+        <label>Number of players: </label>
+        <input
         type='text'
         name='number'
-      />
-      <label>Gameplay time: </label>
-      <input
+        value={number}
+        onChange={event =>setNumber(event.target.value)}
+        />
+        <label>Gameplay time: </label>
+        <input
         type='time'
         name='time'
-      />
-      <label>Official Rule: </label>
-      <input
+        />
+        <label>Official Rule: </label>
+        <input
         type='text'
         name='officialRule'
-      />
-      <label>Interpretation of Rule: </label>
-      <input
+        />
+        <label>Interpretation of Rule: </label>
+        <input
         type='text'
         name='InterpretationRule'
-      />
+        />
 
         <button>SUBMIT</button>
       </form>
-    </section>
-  )
+      </section>
+  )}
+
+  // function useLocalStorage(key, initialValue) {
+  // // State to store our value
+  // // Pass initial state function to useState so logic is only executed once
+  // const [storedValue, setStoredValue] = useState(() => {
+  //   try {
+  //     // Get from local storage by key
+  //     const item = window.localStorage.getItem(key);
+  //     // Parse stored json or if none return initialValue
+  //     return item ? JSON.parse(item) : initialValue;
+  //   } catch (error) {
+  //     // If error also return initialValue
+  //     console.log(error);
+  //     return initialValue;
+  //   }
+  // });
+
+function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item): initialValue
+    } catch (error) {
+      console.log(error)
+      return initialValue;
+    }
+  })
 }
+
+// const Image = (props) => {
+//   const [width, setWidth] = React.useState(0);
+//   React.useEffect(() => {
+//     setWidth(window.innerWidth);
+//   });
+//   return <img src={props.src} style={{ width: width }} />;
+// };
+
 
 export default RuleDisputeForm;
 
