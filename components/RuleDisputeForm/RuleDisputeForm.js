77ed0906924a6eab07841829838bd93b3248e39f
Michael Walker <Michael.e.walker.87@gmail.com>
2020-11-02 17:34:35 -0700

    Removed addtional code

--- components/RuleDisputeForm/RuleDisputeForm.js
+++ components/RuleDisputeForm/RuleDisputeForm.js
@@ -15,63 +15,11 @@ const RuleDisputeForm = () => {
     interpretation: '',
   });
 
+  return(
 
-const RuleDisputeForm = () => {
-  const [expansion, setExpansion] = useLocalStorage('expansion', 'setExpansion')
-  // const [number, setNumber] = useLocalStorage('number', 'setNumber')
-    return(
-      <section>
-      <form>
-        <label>Expansion Name: </label>
-        <input
-        type='text'
-        name='expansion'
-        value={expansion}
-        onChange={event =>setExpansion(event.target.value)}
-        />
-        <label>Number of players: </label>
-        <input
-        type='text'
-        name='number'
-        value={number}
-        onChange={event =>setNumber(event.target.value)}
-        />
-        <label>Gameplay time: </label>
-        <input
-        type='time'
-        name='time'
-        />
-        <label>Official Rule: </label>
-        <input
-        type='text'
-        name='officialRule'
-        />
-        <label>Interpretation of Rule: </label>
-        <input
-        type='text'
-        name='InterpretationRule'
-        />
-
-        <button>SUBMIT</button>
-      </form>
-      </section>
-  )}
-
-
-function useLocalStorage(key, initialValue) {
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key)
-      return item ? JSON.parse(item): initialValue
-    } catch (error) {
-      console.log(error)
-      return initialValue;
-    }
-  })
+  )
+  
 }
-
-
-
 export default RuleDisputeForm;
 
 //eventually upvote, downvote, and link
