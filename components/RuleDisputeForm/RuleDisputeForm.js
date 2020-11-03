d7583248bdd5dca81decc6bece3a6b765e4cf784
Michael Walker <Michael.e.walker.87@gmail.com>
2020-11-02 17:43:06 -0700

    Add the JSX onChange

--- components/RuleDisputeForm/RuleDisputeForm.js
+++ components/RuleDisputeForm/RuleDisputeForm.js
@@ -22,26 +22,33 @@ const RuleDisputeForm = () => {
       <input
       type='text'
       name='expansion'
+      value={expansion}
+      onChange={event => setExpansion(event.target.value)}
       />
       <label>Number of players: </label>
       <input
       type='number'
       name='number'
+      value={players}
+      onChange={event => setPlayers(parseInt(event.target.value))}
       />
       <label>Gameplay time in mintues: </label>
       <input
       type='number'
       name='time'
+      onChange={event => setTime(parseTime(event.target.value))}
       />
       <label>Official Rule: </label>
       <input
       type='text'
       name='officialRule'
+      onChange={event => setOfficialRule(event.target.value)}
       />
       <label>Interpretation of Rule: </label>
       <input
       type='text'
       name='InterpretationRule'
+      onChange={event => setInterpretation(event.target.value)}
       />
       <button onClick={(event) => submitForm(event)}>SUBMIT</button>
     </form>
