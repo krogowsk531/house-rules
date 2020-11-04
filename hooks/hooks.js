71e17c0309c6ba4c5bcfa82c74206ed226efc890
Hanna Kim <hannakim91@gmail.com>
2020-11-04 16:20:44 -0700

    Alignment issues

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -1,15 +1,15 @@
 import React, {useState} from 'react'
 
 export const useLocalStorage = (key, initialValue) => {
-    const [storedValue, setStoredValue] = useState(() => {
-        try {
-          const item = window.localStorage.getItem(key);
-          return item ? JSON.parse(item) : initialValue;
-        } catch (error) {
-          console.log(error);
-          return initialValue;
-        }
-      });
-      return [storedValue, setStoredValue]
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+  return [storedValue, setStoredValue]
 }
 
