16f04b3569a22801ed91d6b4065d4931c98a4e90
Hanna Kim <hannakim91@gmail.com>
2020-11-04 16:35:02 -0700

    Mve custom hooks to hooks directory

--- components/RuleDisputes/RuleDisputes.js
+++ components/RuleDisputes/RuleDisputes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import Comments from '../Comments/Comments.js'
-// import { useLocalStorage } from '../../hooks/hooks.js';
+import { useAllDisputeComments } from '../../hooks/hooks.js';
 
 const RuleDisputeForm = () => {
   const [expansion, setExpansion] = useState('');
@@ -85,37 +85,7 @@ const RuleDisputeForm = () => {
   )
 }
 
-function useLocalStorage(key, initialValue) {
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window ? window.localStorage.getItem(key) : null
-      return item ? JSON.parse(item) : initialValue
-    } catch (error) {
-      console.log(error)
-      return initialValue
-    }
-  })
-  return [storedValue, setStoredValue]
-}
 
-function useAllDisputeComments(key) {
-  const [ storedValue, setStoredValue ] = useLocalStorage('allDisputes')
-
-  const setValue = value => {
-    try {
-      setStoredValue(value)
-      let itemToSet = [value]
-      if (storedValue) {
-        const parsedStoredValue = JSON.parse(window.localStorage.getItem('allDisputes'))
-        itemToSet = [...parsedStoredValue, value]
-      }
-      window.localStorage.setItem(key, JSON.stringify(itemToSet));  
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  return [storedValue, setValue]
-}
 
 export default RuleDisputeForm;
 
--- hooks/hooks.js
+++ hooks/hooks.js
@@ -1,15 +1,34 @@
 import React, {useState} from 'react'
 
-export const useLocalStorage = (key, initialValue) => {
+function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(() => {
     try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
+      const item = window ? window.localStorage.getItem(key) : null
+      return item ? JSON.parse(item) : initialValue
     } catch (error) {
-      console.log(error);
-      return initialValue;
+      console.log(error)
+      return initialValue
     }
-  });
+  })
   return [storedValue, setStoredValue]
+}
+
+export function useAllDisputeComments(key) {
+  const [ storedValue, setStoredValue ] = useLocalStorage('allDisputes')
+
+  const setValue = value => {
+    try {
+      setStoredValue(value)
+      let itemToSet = [value]
+      if (storedValue) {
+        const parsedStoredValue = JSON.parse(window.localStorage.getItem('allDisputes'))
+        itemToSet = [...parsedStoredValue, value]
+      }
+      window.localStorage.setItem(key, JSON.stringify(itemToSet));  
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return [storedValue, setValue]
 }
 
