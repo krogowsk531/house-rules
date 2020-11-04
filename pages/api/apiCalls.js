3edf63691733cb65ca75c95b9ff55c74bcc4404c
Hanna Kim <hannakim91@gmail.com>
2020-11-03 17:33:49 -0700

    Delete comments

--- pages/api/apiCalls.js
+++ pages/api/apiCalls.js
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export const getGames = async () => {
   const promise =  await fetch(`https://api.boardgameatlas.com/api/search?order_by=popularity&client_id=REACT_APP_KEYCODE`)
   return await promise.json()
